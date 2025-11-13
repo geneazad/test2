97399ccf9a35e14dbd5d8bc332cb17a5280d717d
geneazad <geneazad@gmail.com>
2025-11-12 21:52:30 -0500

    project3

---
+++ project3/app.js
@@ -0,0 +1,128 @@
+// ########################################
+// ########## SETUP
+
+// Express
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const PORT = 31231;
+
+// Database
+const db = require('./database/db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars'); // Import express-handlebars engine
+app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
+app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
+
+// ########################################
+// ########## ROUTE HANDLERS
+
+// READ ROUTES
+app.get('/', async function (req, res) {
+    try {
+        res.render('home'); // Render the home.hbs file
+    } catch (error) {
+        console.error('Error rendering page:', error);
+        // Send a generic error message to the browser
+        res.status(500).send('An error occurred while rendering the page.');
+    }
+});
+
+
+//############
+//## CUSTOMERS
+
+// READ
+app.get('/customers', async (req, res) => {
+    try {
+        const [customers] = await db.query('SELECT * FROM customers;');
+        res.render('customers', { customers });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send('An error occurred while executing database queries');
+    }
+});
+
+
+//###########
+//## PRODUCTS
+
+// READ
+app.get('/products', async (req, res) => {
+  try {
+    const [products] = await db.query(`
+      SELECT p.id_product, p.product_name, p.price, p.inventory_quantity, c.category_name AS category
+      FROM products p
+      LEFT JOIN categories c ON p.category_id_category = c.id_category
+    `);
+    const [categories] = await db.query('SELECT * FROM categories');
+    res.render('products', { products, categories });
+  } catch (error) {
+    console.error('Error executing queries:', error);
+    res.status(500).send('An error occurred while executing database queries');
+  }
+});
+
+
+// ########
+//## ORDERS
+
+// READ
+app.get('/orders', async (req, res) => {
+    try {
+        const [orders] = await db.query(`
+            SELECT o.id_order, o.order_date, o.total_amount,
+                   c.cust_name AS customer
+            FROM orders o
+            JOIN customers c ON o.customers_id_customer = c.id_customer;
+        `);
+        const [customers] = await db.query('SELECT * FROM customers;');
+        res.render('orders', { orders, customers });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send('An error occurred while executing database queries.');
+    }
+});
+
+
+//#################
+//## PRODUCT_ORDERS 
+
+// READ
+app.get('/product_orders', async (req, res) => {
+    try {
+        const [po] = await db.query(`
+            SELECT po.products_id_product AS product_id,
+                   p.product_name,
+                   po.orders_id_order AS order_id,
+                   o.order_date,
+                   po.quantity
+            FROM product_orders po
+            JOIN products p ON po.products_id_product = p.id_product
+            JOIN orders o ON po.orders_id_order = o.id_order;
+        `);
+        const [products] = await db.query('SELECT * FROM products;');
+        const [orders] = await db.query('SELECT * FROM orders;');
+        res.render('product_orders', { product_orders: po, products, orders });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send('Error loading product orders.');
+    }
+});
+
+
+// ########################################
+// ########## LISTENER
+
+app.listen(PORT, function () {
+    console.log(
+        'Express started on http://localhost:' +
+            PORT +
+            '; press Ctrl-C to terminate.'
+    );
+});
+
---
+++ project3/database/db-connector.js
@@ -0,0 +1,17 @@
+// Get an instance of mysql we can use in the app
+let mysql = require('mysql2')
+
+// Create a 'connection pool' using the provided credentials
+const pool = mysql.createPool({
+    waitForConnections: true,
+    connectionLimit   : 10,
+    host              : 'classmysql.engr.oregonstate.edu',
+    user              : 'cs340_azadg',
+    password          : 'x11K0tU5GqK9',
+    database          : 'cs340_azadg'
+}).promise(); // This makes it so we can use async / await rather than callbacks
+
+// Export it for use in our application
+module.exports = pool;
+
+
